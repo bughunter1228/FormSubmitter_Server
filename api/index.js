5d73c7bebd2bc7adfe763e4a733e5a141583290b
wintershine-1030 <wintershine1030@gmail.com>
2025-07-23 04:45:41 +0300

    Update group array

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const TelegramBot = require('node-telegram-bot-api');
+const express = require("express");
+const cors = require("cors");
+const TelegramBot = require("node-telegram-bot-api");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = 5000;
@@ -13,96 +13,92 @@ app.use(express.json());
 app.use(cors());
 
 const fields = {
-    'resort_touring': 'Resort Touring',
-    'tour_date': 'Tour Date',
-    'tour_time': 'Tour Time',
-    'marital_status': 'Marital Status',
-    'credit_debit': 'Credit Card or Debit Card',
-    'guest1': 'Guest #1',
-    'guest2': 'Guest #2',
-    'pn_guest1': 'Phone number Guest #1',
-    'pn_guest2': 'Phone number Guest #2',
-    'street_address': 'Street Address',
-    'city': 'City',
-    'state': 'State',
-    'zip_code': 'Zip Code',
-    'email': 'Email',
-    'deposit': 'Deposit',
-    'gifts': 'Gifts',
-    'opc_name': 'OPC Name',
-    'greeter_name': 'Greeter Name',
-    'location_booked': 'Location Booked',
-    'notes': 'Notes' 
+  resort_touring: "Resort Touring",
+  tour_date: "Tour Date",
+  tour_time: "Tour Time",
+  marital_status: "Marital Status",
+  credit_debit: "Credit Card or Debit Card",
+  guest1: "Guest #1",
+  guest2: "Guest #2",
+  pn_guest1: "Phone number Guest #1",
+  pn_guest2: "Phone number Guest #2",
+  street_address: "Street Address",
+  city: "City",
+  state: "State",
+  zip_code: "Zip Code",
+  email: "Email",
+  deposit: "Deposit",
+  gifts: "Gifts",
+  opc_name: "OPC Name",
+  greeter_name: "Greeter Name",
+  location_booked: "Location Booked",
+  notes: "Notes",
+};
 
-}
+const GroupIds = [-4936941796];
 
 bot.onText(/\/start/, async (msg, match) => {
-    try {
-        const chatId = msg.chat.id;
-        const firstName = msg.from.first_name || "there";
+  try {
+    const chatId = msg.chat.id;
+    const firstName = msg.from.first_name || "there";
 
-        const appUrl = 'https://form-submitter-mini-app.vercel.app/';
+    const appUrl = "https://form-submitter-mini-app.vercel.app/";
 
-        const welcomeMessage = `Hello ${firstName}, Let's start submitting`;
+    const welcomeMessage = `Hello ${firstName}, Let's start submitting`;
 
-        const opts = {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        { text: 'Start', web_app: { url: appUrl } }
-                    ]
-                ]
-            }
-        };
+    const opts = {
+      reply_markup: {
+        inline_keyboard: [[{ text: "Start", web_app: { url: appUrl } }]],
+      },
+    };
 
-        bot.sendMessage(chatId, welcomeMessage, opts);
-    } catch (error) {
-        console.log("Error occured: ", error.message);
-    }
+    bot.sendMessage(chatId, welcomeMessage, opts);
+  } catch (error) {
+    console.log("Error occured: ", error.message);
+  }
 });
 
-const getSavedDataMessage = async (data) =>{
-    const entries = Object.entries(fields);
-    let message = '';
+const getSavedDataMessage = async (data) => {
+  const entries = Object.entries(fields);
+  let message = "";
 
-    // Print each key-value pair
-    entries.forEach(([key, value]) => {
-        if(data[key].length) {
-            message = `${message}\n✅ ${value}: ${data[key]}`
-        } else {
-            message = `${message}\n☑️ ${value}:`
-        }
+  // Print each key-value pair
+  entries.forEach(([key, value]) => {
+    if (data[key].length) {
+      message = `${message}\n✅ ${value}: ${data[key]}`;
+    } else {
+      message = `${message}\n☑️ ${value}:`;
+    }
+  });
+
+  return message;
+};
+
+app.post("/send", async (req, res) => {
+  const data = req.body;
+
+  try {
+    const message = await getSavedDataMessage(data);
+
+    // Wait for Telegram to respond
+    GroupIds.forEach(async (groupId) => {
+      await bot.sendMessage(groupId, message);
     });
 
-    return message
-}
+    res.status(200).json({
+      status: "success",
+      message: "Message sent successfully",
+    });
+  } catch (error) {
+    console.error("Telegram error:", error);
 
-
-app.post('/send', async (req, res) => {
-    const data = req.body;
-
-    try {
-        const message = await getSavedDataMessage(data);
-
-        // Wait for Telegram to respond
-        const telegramResponse = await bot.sendMessage(process.env.GROUP_ID, message);
-
-        res.status(200).json({
-            status: 'success',
-            message: 'Message sent successfully',
-            telegramMessageId: telegramResponse.message_id,
-        });
-    } catch (error) {
-        console.error('Telegram error:', error);
-
-        res.status(500).json({
-            status: 'error',
-            message: 'Failed to send Telegram message'
-        });
-    }
+    res.status(500).json({
+      status: "error",
+      message: "Failed to send Telegram message",
+    });
+  }
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Scraper server running at http://localhost:${PORT}`);
+  console.log(`Scraper server running at http://localhost:${PORT}`);
 });
